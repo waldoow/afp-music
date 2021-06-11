259681d8aa5fd4b573096ca12fea09eaa68f92cc
dylan <dylan@lil-fmu200030.home>
2021-06-11 13:35:03 +0200

    coucou

---
+++ music.fresh/dylantest.swift
@@ -0,0 +1,20 @@
+//
+//  dylantest.swift
+//  music.fresh
+//
+//  Created by dylan on 11/06/2021.
+//
+
+import SwiftUI
+
+struct dylantest: View {
+    var body: some View {
+        Text("Test")
+    }
+}
+
+struct dylantest_Previews: PreviewProvider {
+    static var previews: some View {
+        dylantest()
+    }
+}
