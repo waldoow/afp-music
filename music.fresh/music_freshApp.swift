ecb4075e9b34fed5d85b48e1e4b4895863671643
waldow <alisahraoui.walid@gmail.com>
2021-06-11 10:37:29 +0200

    Initial Commit

---
+++ music.fresh/music_freshApp.swift
@@ -0,0 +1,17 @@
+//
+//  music_freshApp.swift
+//  music.fresh
+//
+//  Created by A_W on 11/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct music_freshApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
