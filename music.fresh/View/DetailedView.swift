07ce6e9c862969ed694bc890c024443b8f9b33a0
dylan <dylan@lil-fmu200030.home>
2021-06-12 21:57:21 +0200

    Modification de la vue

--- music.fresh/View/DetailedView.swift
+++ music.fresh/View/DetailedView.swift
@@ -8,13 +8,36 @@
 import SwiftUI
 
 struct DetailedView: View {
+    
+    let artist: Artist
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading) {
+            ZStack {
+                Image(artist.imageName).resizable().scaledToFit()
+                    .ignoresSafeArea()
+                Text("Klemn Schen").bold()
+                    .foregroundColor(.white)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .font(.largeTitle)
+                    .padding(.top, 150)
+                }
+            
+            Text("Biographie")
+                .underline()
+                .bold()
+                .foregroundColor(.gray)
+                .font(.title3)
+                .padding(.leading)
+            
+            Text(artist.description).padding(.horizontal)
+                
+            Spacer()
+        }
     }
 }
 
 struct DetailedView_Previews: PreviewProvider {
     static var previews: some View {
-        DetailedView()
+        DetailedView(artist: artist1)
     }
 }
