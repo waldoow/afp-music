79ebf27f01a388bce78228d054b4c21aac48dab6
dylan <dylan@lil-fmu200030.home>
2021-06-12 21:22:48 +0200

    Ajout du fichier dataArtist

---
+++ music.fresh/View/DetailedView.swift
@@ -0,0 +1,20 @@
+//
+//  DetailedView.swift
+//  music.fresh
+//
+//  Created by dylan on 12/06/2021.
+//
+
+import SwiftUI
+
+struct DetailedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DetailedView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailedView()
+    }
+}
