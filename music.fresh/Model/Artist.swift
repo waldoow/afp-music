fa312c3f957d1443a38b355d3cb5b05260488beb
mounir-o <mounir69212@gmail.com>
2021-06-14 15:54:27 +0200

    Ajout model Artist

---
+++ music.fresh/Model/Artist.swift
@@ -0,0 +1,15 @@
+//
+//  Artist.swift
+//  music.fresh
+//
+//  Created by mac32 on 14/06/2021.
+//
+
+import Foundation
+
+struct Artist {
+    let name: String
+    let imageName: String
+    let description: String
+    // let artistSong: [Song]
+}
