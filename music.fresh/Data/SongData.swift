ef8bbf2e85a1ebf103bc4faf3a615bbb375775d5
mounir-o <mounir69212@gmail.com>
2021-06-14 10:23:42 +0200

    Ajout fichier songList modification fichier songData

--- music.fresh/Data/SongData.swift
+++ music.fresh/Data/SongData.swift
@@ -7,6 +7,8 @@
 
 import Foundation
 
-let song1 = Song(title: "Titre 1", artist: "Artiste", description: "Album", imageName: "", url: "", year: 2021)
-let song2 = Song(title: "Titre 2", artist: "Artiste", description: "Album", imageName: "", url: "", year: 2021)
-let song3 = Song(title: "Titre 3", artist: "Artiste", description: "Album", imageName: "", url: "", year: 2021)
+let song1 = Song(title: "Titre 1", artist: "Artiste", description: "Album", imageName: "imageSong", url: "", year: 2021)
+let song2 = Song(title: "Titre 2", artist: "Artiste", description: "Album", imageName: "imageSong", url: "", year: 2021)
+let song3 = Song(title: "Titre 3", artist: "Artiste", description: "Album", imageName: "imageSong", url: "", year: 2021)
+
+let songsList = [song1, song2, song3]
