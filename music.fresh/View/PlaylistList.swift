7dc0ba3d9893b0ae6d1f284424313724248b4875
mounir-o <mounir69212@gmail.com>
2021-06-14 12:11:39 +0200

    Ajout vue PlaylistList

---
+++ music.fresh/View/PlaylistList.swift
@@ -0,0 +1,34 @@
+//
+//  PlaylistList.swift
+//  music.fresh
+//
+//  Created by mac32 on 14/06/2021.
+//
+
+import SwiftUI
+
+struct PlaylistList: View {
+    let playlists: [Playlist]
+    var body: some View {
+        HStack{
+            List(playlists) { playlist in
+                Image(playlist.imageName)
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 55, height: 55)
+                VStack(alignment: .leading){
+                Text(playlist.title)
+                    .font(.title3)
+                Text(playlist.user)
+                    .foregroundColor(.secondary)
+                }
+            }
+        }
+    }
+}
+
+struct PlaylistList_Previews: PreviewProvider {
+    static var previews: some View {
+        PlaylistList(playlists: playlistsList)
+    }
+}
