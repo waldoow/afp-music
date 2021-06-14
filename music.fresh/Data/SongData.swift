e7fbc011567862905512494d6954bae5099d4149
mounir-o <mounir69212@gmail.com>
2021-06-14 10:05:13 +0200

    Ajout du fichier SongData

---
+++ music.fresh/Data/SongData.swift
@@ -0,0 +1,12 @@
+//
+//  SongData.swift
+//  music.fresh
+//
+//  Created by mac32 on 14/06/2021.
+//
+
+import Foundation
+
+let song1 = Song(title: "Titre 1", artist: "Artiste", description: "Album", imageName: "", url: "", year: 2021)
+let song2 = Song(title: "Titre 2", artist: "Artiste", description: "Album", imageName: "", url: "", year: 2021)
+let song3 = Song(title: "Titre 3", artist: "Artiste", description: "Album", imageName: "", url: "", year: 2021)
