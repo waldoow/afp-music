23afbba635c3c827733549e285b31becbe5d9b8a
lucie <lucie.besson@gmail.com>
2021-06-14 17:37:15 +0200

    created UserView

---
+++ music.fresh/View/UserView.swift
@@ -0,0 +1,20 @@
+//
+//  UserView.swift
+//  music.fresh
+//
+//  Created by lucie on 14/06/2021.
+//
+
+import SwiftUI
+
+struct UserView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct UserView_Previews: PreviewProvider {
+    static var previews: some View {
+        UserView()
+    }
+}
