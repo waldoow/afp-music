13eac6fc335c6c9703971c03b28a7e23888af8d3
mounir-o <mounir69212@gmail.com>
2021-06-14 17:06:26 +0200

    Création de la vue ArtistList

---
+++ music.fresh/View/ArtistList.swift
@@ -0,0 +1,40 @@
+//
+//  ArtistList.swift
+//  music.fresh
+//
+//  Created by mac32 on 14/06/2021.
+//
+
+import SwiftUI
+
+struct ArtistList: View {
+    let artists: [Artist]
+    var body: some View {
+        HStack{
+            List(artists) { artist in
+                Image(artist.imageName)
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 55, height: 55)
+                VStack(alignment: .leading){
+                    Text(artist.name)
+                        .font(.title3)
+                    Text("Artiste")
+                        .foregroundColor(.secondary)
+                }
+                Spacer()
+                Button(action: {
+                }, label: {
+                    Image(systemName: "greaterthan")
+                        .foregroundColor(.gray)
+                })
+            }
+        }
+    }
+}
+
+struct ArtistList_Previews: PreviewProvider {
+    static var previews: some View {
+        ArtistList(artists: artistsList)
+    }
+}
