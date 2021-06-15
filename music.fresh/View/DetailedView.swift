41b4023d591d70542dc7f7fdd622a6e2b75719bd
waldow <alisahraoui.walid@gmail.com>
2021-06-15 12:43:32 +0200

    Some changes

--- music.fresh/View/DetailedView.swift
+++ music.fresh/View/DetailedView.swift
@@ -8,54 +8,82 @@
 import SwiftUI
 
 struct DetailedView: View {
+    var artist: Artist
     
-    let artist: Artist
-    
-    var columns: [GridItem] =
-        Array(repeating: .init(.flexible()), count: 3)
-    
-    
-    var body: some View {
-        VStack(alignment: .leading) {
-            ZStack {
-                Image(artist.imageName).resizable()
-                    .scaledToFit()
-                    // .aspectRatio(contentMode: .fit)
-                    .ignoresSafeArea()
-            
-                
-                Text(artist.name).bold()
-                    .foregroundColor(.white)
-                    .font(.title)
-                    .padding(.top, 130)
-            }
-            
-            Text("Biographie").bold()
-                .underline()
-                .foregroundColor(.gray)
-                .padding(.leading)
-                .font(.title2)
-            
-            Text(artist.description).padding([.horizontal, .top, .bottom], 15)
-                .font(.title3)
-            
-            Text("Titres").bold()
-                .underline()
-                .foregroundColor(.gray)
-                .padding(.leading)
-                .font(.title2)
-            
-            LazyVGrid(columns: columns) {
-                SongListCell()
-                SongListCell()
-                SongListCell()
-                
-            }
-            
-            Spacer()
+    var songs: [Song] {
+        songsList.filter { song in
+            // ca aurait du etre id, mais comme on genere un nouvelle uuid a chaque instance ca ne fonctionne pas :(
+            return song.artist.name == artist.name
         }
     }
-    
+
+    var body: some View {
+        NavigationView {
+            
+        }
+        .overlay(
+            VStack {
+                Image(artist.imageName)
+                    .resizable()
+                    .scaledToFit()
+                    .overlay(
+                        VStack {
+                            Spacer()
+                            HStack {
+                                Text(artist.name).bold()
+                                    .foregroundColor(.white)
+                                    .font(.system(size:50, weight: .bold))
+                                
+                                Spacer()
+                            }
+                            .padding(5)
+                        }
+                    )
+                
+                ScrollView(.vertical, showsIndicators: false, content: {
+                    VStack {
+                        VStack(alignment: .leading) {
+                            Text("Biographie").bold()
+                                .foregroundColor(.gray)
+                                .padding(.leading)
+                                .font(.title2)
+                            
+                            Text(artist.description).padding([.horizontal, .top, .bottom], 15)
+                                .font(.callout)
+                        }.padding()
+                        
+                        VStack(alignment: .leading) {
+                            Text("Titres").bold()
+                                .foregroundColor(.gray)
+                                .padding(.leading)
+                                .font(.title2)
+                            
+                            ScrollView(.horizontal, showsIndicators: false, content: {
+                                HStack(spacing: 20) {
+                                    ForEach(songs, id: \.id, content: { song in
+                                        Image(song.imageName)
+                                            .resizable()
+                                            .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                                            .overlay(
+                                                NavigationLink(
+                                                    destination: Text("Player"),
+                                                    label: {
+                                                        Image(systemName: "play")
+                                                            .font(.system(size: 35, weight: .semibold))
+                                                            .foregroundColor(.white)
+                                                    })
+                                            )
+                                    })
+                                }
+                            }).padding()
+                        }
+                        
+                        Spacer()
+                    }
+                })
+            })
+        .edgesIgnoringSafeArea(.top)
+    }
 }
 
 struct DetailedView_Previews: PreviewProvider {
