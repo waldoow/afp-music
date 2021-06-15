e5b2a0e885abaff0dcde554fd230f0a439de4d14
mounir-o <mounir69212@gmail.com>
2021-06-15 12:06:50 +0200

    Ajout d'une ScrollView et une struct selectionsButton

--- music.fresh/View/SearchView.swift
+++ music.fresh/View/SearchView.swift
@@ -9,7 +9,21 @@ import SwiftUI
 
 struct SearchView: View {
     @State private var selection = 0
+    //    @State var currentTab = "Titres"
+    //    @Namespace var animation
     var body: some View {
+        //                VStack(alignment: .center, spacing: 0){
+        //                    GeometryReader { geometry in
+        //                        ScrollView(.horizontal, showsIndicators: false, content: {
+        //                            HStack(alignment: .center, spacing: 0){
+        //                                selectionsButton(title: "Titres", currentTab: $currentTab, animation: animation)
+        //                                selectionsButton(title: "Playlists", currentTab: $currentTab, animation: animation)
+        //                                selectionsButton(title: "Artistes", currentTab: $currentTab, animation: animation)
+        //                            }
+        //                            .frame(width: geometry.size.width)
+        //                        })
+        //                    }
+        //                    .padding(.top, 20)
         VStack{
             Picker("", selection: $selection) {
                 Text("Titres").tag(0)
@@ -28,9 +42,41 @@ struct SearchView: View {
         }
     }
 }
-
+//    }
+//}
 struct SearchView_Previews: PreviewProvider {
     static var previews: some View {
         SearchView()
     }
 }
+
+//struct selectionsButton: View {
+//    var title: String
+//    @Binding var currentTab: String
+//    var animation: Namespace.ID
+//    var body: some View {
+//        Button(action: {
+//            withAnimation {
+//                currentTab = title
+//            }
+//        }, label: {
+//            LazyVStack(spacing: 12){
+//                Text(title).fontWeight(.semibold)
+//
+//                    .foregroundColor(currentTab == title ? .yellow : .gray)
+//                    .padding(.horizontal)
+//
+//                if currentTab == title {
+//                    Capsule()
+//                        .fill(Color.yellow)
+//                        .frame(height: 1.2)
+//                        .matchedGeometryEffect(id: "TAB", in: animation)
+//                } else {
+//                    Capsule()
+//                        .fill(Color.clear)
+//                        .frame(height: 1.2)
+//                }
+//            }
+//        })
+//    }
+//}
