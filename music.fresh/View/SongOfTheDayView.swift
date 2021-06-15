8b28efa45904325a31f24667f242911c74582d9b
Pablo <sam.prog.74@gmail.com>
2021-06-15 09:31:24 +0200

    création branche + fichier SongOfThDayView

---
+++ music.fresh/View/SongOfTheDayView.swift
@@ -0,0 +1,51 @@
+//
+//  SongOfTheDayView.swift
+//  music.fresh
+//
+//  Created by SPGJ on 14/06/2021.
+//
+
+import SwiftUI
+
+struct SongOfTheDayView: View {
+    var body: some View {
+        VStack{
+            VStack{
+                Image(systemName: "plus.circle")
+                                .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                                .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .trailing)
+                            Text("Chanson du jour")
+                                .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .leading)
+                                .font(.title)
+            }
+            Spacer()
+            Text("Découvrez de la vraie nouveauté, peu connu, que vous ne trouvez pas sur les autres apps.")
+            Spacer()
+            Text("AudioPlayerView")
+                .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, maxHeight:150)
+                .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                                .background(Color.yellow)
+            ZStack{
+                Image(artist1.imageName)
+                    .frame(maxWidth: .infinity)
+                    .aspectRatio(contentMode: .fill)
+                Text(artist1.name)
+                    .foregroundColor(.white)
+                    .offset(y: 60)
+            }
+            Text("VoteView")
+                .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, maxHeight:150)
+                .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                .background(Color.yellow)
+                
+        }
+        .padding()
+    }
+}
+
+struct SongOfTheDayView_Previews: PreviewProvider {
+    static var previews: some View {
+        SongOfTheDayView()
+    }
+}
+
