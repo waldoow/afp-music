de8401ba82a3e2457090766a136752845bf1f059
mounir-o <mounir69212@gmail.com>
2021-06-15 14:48:38 +0200

    Ajout des 3 listes dans la searchView

--- music.fresh/View/SearchView.swift
+++ music.fresh/View/SearchView.swift
@@ -13,38 +13,26 @@ struct SearchView: View {
     @Namespace var animation
     var body: some View {
         VStack(alignment: .center, spacing: 0){
-            GeometryReader { geometry in
-                ScrollView(.horizontal, showsIndicators: false, content: {
-                    HStack(alignment: .center, spacing: 0){
-                        selectionsButton(title: "Titres", currentTab: $currentTab, animation: animation)
-                        selectionsButton(title: "Playlists", currentTab: $currentTab, animation: animation)
-                        selectionsButton(title: "Artistes", currentTab: $currentTab, animation: animation)
-                    }
-                    .frame(width: geometry.size.width)
-                })
+            HStack(alignment: .center, spacing: 0){
+                selectionsButton(title: "Titres", currentTab: $currentTab, animation: animation)
+                selectionsButton(title: "Playlists", currentTab: $currentTab, animation: animation)
+                selectionsButton(title: "Artistes", currentTab: $currentTab, animation: animation)
+            }
+            VStack{
+                if currentTab == "Titres" {
+                    SongList(songs: songsList)
+                }
+                if currentTab == "Playlists" {
+                    PlaylistList(playlists: playlistsList)
+                }
+                if currentTab == "Artistes" {
+                    ArtistList(artists: artistsList)
+                }
             }
             .padding(.top, 20)
-            //        VStack{
-            //            Picker("", selection: $selection) {
-            //                Text("Titres").tag(0)
-            //                Text("Playlists").tag(1)
-            //                Text("Artistes").tag(2)
-            //            }
-            //            .pickerStyle(SegmentedPickerStyle()).padding(.horizontal)
-            //            if selection == 0 {
-            //                SongList(songs: songsList)
-            //            } else if selection == 1 {
-            //                PlaylistList(playlists: playlistsList)
-            //            } else if selection == 2 {
-            //                ArtistList(artists: artistsList)
-            //            }
-            //            Spacer()
-            //        }
         }
     }
 }
-//    }
-//}
 struct SearchView_Previews: PreviewProvider {
     static var previews: some View {
         SearchView()
