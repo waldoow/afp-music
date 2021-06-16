917306fc65979d82cbe6df9b696c979527c40602
Pablo <sam.prog.74@gmail.com>
2021-06-16 10:35:46 +0200

    ajout fichier AddSongOfTheDayView

---
+++ music.fresh/View/AddSongOfTheDayView.swift
@@ -0,0 +1,24 @@
+//
+//  AddSongOfTheDayView.swift
+//  music.fresh
+//
+//  Created by SPGJ on 16/06/2021.
+//
+
+import SwiftUI
+
+struct AddSongOfTheDayView: View {
+    var body: some View {
+        
+        VStack {
+            Text("AddSongOfTheDayView")
+            Text("(modale)")
+        }
+    }
+}
+
+struct AddSongOfTheDayView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddSongOfTheDayView()
+    }
+}
