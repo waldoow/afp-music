d0841d17cffdd94d47e6eb57b6b8c93dbbeff45e
Pablo <sam.prog.74@gmail.com>
2021-06-17 09:25:41 +0200

    mise en forme AddSongOfTheDayView

--- music.fresh/View/AddSongOfTheDayView.swift
+++ music.fresh/View/AddSongOfTheDayView.swift
@@ -8,17 +8,54 @@
 import SwiftUI
 
 struct AddSongOfTheDayView: View {
+    
+    @State var name: String
+    @State var title: String
+    @State var url: String
+    @State var year: String
+    @State var description: String
+    
     var body: some View {
         
-        VStack {
-            Text("AddSongOfTheDayView")
-            Text("(modale)")
+        VStack(alignment: .leading) {
+            Image(systemName: "photo")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 200, height: 200, alignment: .leading)
+
+            HStack {
+                Text("Nom :")
+                TextField("Nom", text: $name)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+            }
+            HStack {
+                Text("Titre :")
+                TextField("Titre", text: $title)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+            }
+            HStack {
+                Text("URL :")
+                TextField("URL", text: $url)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+            }
+            HStack{
+                Text("Année :")
+                TextField("Année", text: $year)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+            }
+            HStack {
+                Text("Biographie :")
+                TextField("", text: $description)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+            }
+
         }
+        .padding()
     }
 }
 
 struct AddSongOfTheDayView_Previews: PreviewProvider {
     static var previews: some View {
-        AddSongOfTheDayView()
+        AddSongOfTheDayView(name: "Halestorm", title: "Do Not Disturb", url: "https://www.youtube.com/watch?v=WZNb8A48USo", year: "2018", description: "Halestorm est un groupe de heavy metal américain, originaire de York, en Pennsylvanie. Le groupe est actuellement signé chez Atlantic Records et a réalisé son premier album éponyme en avril 2009. Suit The Strange Case of... en 2012, dont le premier single, Love Bites (So Do I), qui est récompensé d'un Grammy Award dans la catégorie de la meilleure chanson hard rock/metal, le 10 février 20135,6. En 2015 sort l'album Into the Wild Life. Et en 2018 le groupe sort l'album Vicious, avec le single Uncomfortable qui est nommé au Grammy Award dans la catégorie meilleure prestation rock. Ils ont également sorti plusieurs EP, notamment des albums de reprises.")
     }
 }
--- music.fresh/View/SongOfTheDayView.swift
+++ music.fresh/View/SongOfTheDayView.swift
@@ -9,51 +9,62 @@ import SwiftUI
 
 struct SongOfTheDayView: View {
     var body: some View {
-        VStack{
+        NavigationView {
             VStack{
-                Image(systemName: "plus.circle") // bouton ajouter chanson du jour
+                
+                NavigationLink(destination: AddSongOfTheDayView(name: "Halestorm", title: "Do Not Disturb", url: "https://www.youtube.com/watch?v=WZNb8A48USo", year: "2018", description: "Halestorm est un groupe de heavy metal américain, originaire de York, en Pennsylvanie. Le groupe est actuellement signé chez Atlantic Records et a réalisé son premier album éponyme en avril 2009. Suit The Strange Case of... en 2012, dont le premier single, Love Bites (So Do I), qui est récompensé d'un Grammy Award dans la catégorie de la meilleure chanson hard rock/metal, le 10 février 2013. En 2015 sort l'album Into the Wild Life. Et en 2018 le groupe sort l'album Vicious, avec le single Uncomfortable qui est nommé au Grammy Award dans la catégorie meilleure prestation rock. Ils ont également sorti plusieurs EP, notamment des albums de reprises.")){
+
+                }
+                .navigationTitle("Chanson du jour")
+                .navigationBarItems(trailing: Button(action: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/{}/*@END_MENU_TOKEN@*/) {
+                Image(systemName: "plus.circle")
                     .foregroundColor(.blue)
                     .frame(maxWidth: .infinity, alignment: .trailing)
-                Text("Chanson du jour")
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .font(.title)
-            }
-            Spacer()
-            Text("Découvrez de la vraie nouveauté, peu connu, que vous ne trouvez pas sur les autres apps.")
-            Spacer()
-            Text("En attente de la AudioPlayerView")
-                .frame(maxWidth: .infinity, maxHeight:150)
-                .foregroundColor(.blue)
-                .background(Color.yellow)
-            ZStack{
-                Image("user1.jpg")
-                    .resizable()
-                    .frame(maxWidth: .infinity)
-                    .aspectRatio(contentMode: .fit)
-                Text(artist1.name)
-                    .foregroundColor(.white)
-                    .offset(y: 60)
-            }
-            .frame(maxWidth: .infinity, maxHeight:150)
-            
-            ScrollView(.vertical){
+            })
                 
-                VStack{
-                    // mettre tableau vote
-                    VoteView(vote: vote1)
-                        .frame(maxWidth: .infinity, maxHeight:150)
-                    VoteView(vote: vote2)
-                        .frame(maxWidth: .infinity, maxHeight:150)
-                    VoteView(vote: vote3)
-                        .frame(maxWidth: .infinity, maxHeight:150)
-                    VoteView(vote: vote4)
-                        .frame(maxWidth: .infinity, maxHeight:150)
-                    VoteView(vote: vote5)
-                        .frame(maxWidth: .infinity, maxHeight:150)
+                //                VStack{
+                //                    Text("Chanson du jour")
+                //                        .frame(maxWidth: .infinity, alignment: .leading)
+                //                        .font(.title)
+                //                }
+                Spacer()
+                Text("Découvrez de la vraie nouveauté, peu connu, que vous ne trouvez pas sur les autres apps.")
+                Spacer()
+                Text("En attente de la AudioPlayerView")
+                    .frame(maxWidth: .infinity, maxHeight:150)
+                    .foregroundColor(.blue)
+                    .background(Color.yellow)
+                ZStack{
+                    Image("user1.jpg")
+                        .resizable()
+                        .frame(maxWidth: .infinity)
+                    //                    .aspectRatio(contentMode: .fit)
+                    Text(artist1.name)
+                        .foregroundColor(.white)
+                        .offset(y: 60)
+                }
+                .frame(maxWidth: .infinity, maxHeight:150)
+                
+                ScrollView(.vertical){
+                    
+                    VStack{
+                        // mettre tableau vote
+                        VoteView(vote: vote1)
+                            .frame(maxWidth: .infinity, maxHeight:150)
+                        VoteView(vote: vote2)
+                            .frame(maxWidth: .infinity, maxHeight:150)
+                        VoteView(vote: vote3)
+                            .frame(maxWidth: .infinity, maxHeight:150)
+                        VoteView(vote: vote4)
+                            .frame(maxWidth: .infinity, maxHeight:150)
+                        VoteView(vote: vote5)
+                            .frame(maxWidth: .infinity, maxHeight:150)
+                    }
                 }
             }
+            .padding()
         }
-        .padding()
+        //        .edgesIgnoringSafeArea(.top)
     }
 }
 
