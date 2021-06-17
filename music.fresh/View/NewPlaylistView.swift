ba0523a1e0146cd3c58f8708b080e87f147085f3
mounir-o <mounir69212@gmail.com>
2021-06-17 09:54:18 +0200

    Ajout de 2 boutons dans la NewPlaylistView

--- music.fresh/View/NewPlaylistView.swift
+++ music.fresh/View/NewPlaylistView.swift
@@ -19,27 +19,29 @@ struct NewPlaylistView: View {
                 .cornerRadius(15)
                 .opacity(0.2)
                 .overlay(
-                    Image(systemName: "camera.circle.fill")
-                        .resizable()
-                        .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
-                        .frame(width: 80, height: 80)
-                        .foregroundColor(Color(.systemGray2))
+                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                        Image(systemName: "camera.circle.fill")
+                            .resizable()
+                            .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
+                            .frame(width: 80, height: 80)
+                            .foregroundColor(Color.yellow)
+                    })
                 )
                 .padding()
-            
-            TextField("Nom de la playlist", text: $playlistName)
-                .padding(15)
-                .padding(.horizontal, 15)
-            TextField("Description de la playlist", text: $playlistDescription)
-                .padding(15)
-                .padding(.horizontal, 15)
-            HStack {
-                Image(systemName: "plus.circle.fill")
-                TextField("", text: .constant("Ajouter un titre"))
+            Form{
+                Section(content: {
+                    TextField("Nom de la playlist", text: $playlistName)
+                    TextField("Description de la playlist", text: $playlistDescription)
+                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                        HStack{
+                            Image(systemName: "plus.circle.fill")
+                            Text("Ajouter un titre")
+                        }
+                        .foregroundColor(Color.yellow)
+                    })
+                })
+                .padding(8)
             }
-            .foregroundColor(Color.yellow)
-            .padding(15)
-            .padding(.horizontal, 15)
             Spacer()
         }
     }
