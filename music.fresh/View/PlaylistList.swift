2d0ea73ffa89b3355d183bc93d4bf6db7bf1921f
mounir-o <mounir69212@gmail.com>
2021-06-18 16:18:38 +0200

    Modifications PlaylistList

--- music.fresh/View/PlaylistList.swift
+++ music.fresh/View/PlaylistList.swift
@@ -9,45 +9,26 @@ import SwiftUI
 
 struct PlaylistList: View {
     var playlists: [Playlist]
-//    let playlists: [Playlist]
+    //    let playlists: [Playlist]
     @State var text = ""
     var body: some View {
         ScrollView(.vertical, showsIndicators: false) {
             ForEach(playlists.filter({"\($0)".contains(text.lowercased()) || text.isEmpty })) { playlist in
-                HStack{
-                    Image(uiImage: playlist.imageName!)
-                        .resizable()
-                        .frame(width: 55, height: 55)
-                        .clipped()
-                    VStack(alignment: .leading){
-                        Text(playlist.title)
-                            .font(.title3)
-                        Text(playlist.user)
-                            .foregroundColor(.secondary)
-                    }
-                    Spacer()
-                    Button(action: {
-                    }, label: {
-                        Image(systemName: "chevron.right")
-                            .foregroundColor(.gray)
                 NavigationLink(
                     destination: PlaylistView(playlist: playlist),
                     label: {
-                        HStack {
-                            Image(playlist.imageName)
+                        HStack{
+                            Image(uiImage: playlist.imageName!)
                                 .resizable()
                                 .frame(width: 55, height: 55)
                                 .clipped()
-
                             VStack(alignment: .leading){
                                 Text(playlist.title)
                                     .font(.title3)
                                 Text(playlist.user)
                                     .foregroundColor(.secondary)
                             }
-
                             Spacer()
-
                             Button(action: {
                             }, label: {
                                 Image(systemName: "chevron.right")
@@ -55,12 +36,10 @@ struct PlaylistList: View {
                             })
                         }
                     })
-
-            }
-        }.padding(15)
+            }.padding(15)
+        }
     }
 }
-
 struct PlaylistList_Previews: PreviewProvider {
     static var previews: some View {
         PlaylistList(playlists: playlistsList)
