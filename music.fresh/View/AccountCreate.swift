1e5499b65eee8d477cdd2702945eaabe085fa423
VibeDylan <dylanbourbotte62@icloud.com>
2021-06-18 09:29:46 +0200

    modification

---
+++ music.fresh/View/AccountCreate.swift
@@ -0,0 +1,20 @@
+//
+//  AccountCreate.swift
+//  music.fresh
+//
+//  Created by dylan on 17/06/2021.
+//
+
+import SwiftUI
+
+struct AccountCreate: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AccountCreate_Previews: PreviewProvider {
+    static var previews: some View {
+        AccountCreate()
+    }
+}
---
+++ music.fresh/View/AccountLogin.swift
@@ -0,0 +1,20 @@
+//
+//  AccountLogin.swift
+//  music.fresh
+//
+//  Created by dylan on 17/06/2021.
+//
+
+import SwiftUI
+
+struct AccountLogin: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AccountLogin_Previews: PreviewProvider {
+    static var previews: some View {
+        AccountLogin()
+    }
+}
