54548184c187e5e18806141a03051de8b01bc619
mounir-o <mounir69212@gmail.com>
2021-06-23 16:23:47 +0200

    Changement de l'image des playlists, modifications du nom et des titres

--- music.fresh/View/PlaylistView.swift
+++ music.fresh/View/PlaylistView.swift
@@ -9,39 +9,45 @@ import SwiftUI
 
 struct PlaylistView: View {
     var playlist: Playlist
-
+    
     var body: some View {
         VStack {
             VStack {
-                VStack(spacing: 0) {
-                    HStack(spacing: 0) {
-                        Spacer()
-                        Image(playlist.songs.indices.contains(0) ? playlist.songs[0].imageName : "defaultImageSong")
-                            .resizable()
-                            .frame(width: 80, height: 80, alignment: .center)
-                        Image(playlist.songs.indices.contains(1) ? playlist.songs[1].imageName : "defaultImageSong")
-                            .resizable()
-                            .frame(width: 80, height: 80, alignment: .center)
-                        Spacer()
-                    }
-
-                    HStack(spacing: 0) {
-                        Spacer()
-                        Image(playlist.songs.indices.contains(0) ? playlist.songs[0].imageName : "defaultImageSong")
-                            .resizable()
-                            .frame(width: 80, height: 80, alignment: .center)
-                        Image(playlist.songs.indices.contains(1) ? playlist.songs[1].imageName : "defaultImageSong")
-                            .resizable()
-                            .frame(width: 80, height: 80, alignment: .center)
-                        Spacer()
-                    }
-                }
+                //                VStack(spacing: 0) {
+                //                    HStack(spacing: 0) {
+                //                        Spacer()
+                //                        Image(playlist.songs.indices.contains(0) ? playlist.songs[0].imageName : "defaultImageSong")
+                //                            .resizable()
+                //                            .frame(width: 80, height: 80, alignment: .center)
+                //                        Image(playlist.songs.indices.contains(1) ? playlist.songs[1].imageName : "defaultImageSong")
+                //                            .resizable()
+                //                            .frame(width: 80, height: 80, alignment: .center)
+                //                        Spacer()
+                //                    }
+                //                }
+                Image(uiImage: playlist.imageName!)
+                    .resizable()
+                    .frame(width: 250, height: 250, alignment: .center)
                 
-                HStack() {
-                    VStack {
+                
+                //                    HStack(spacing: 0) {
+                //                        Spacer()
+                //                        Image(playlist.songs.indices.contains(0) ? playlist.songs[0].imageName : "defaultImageSong")
+                //                            .resizable()
+                //                            .frame(width: 80, height: 80, alignment: .center)
+                //                        Image(playlist.songs.indices.contains(1) ? playlist.songs[1].imageName : "defaultImageSong")
+                //                            .resizable()
+                //                            .frame(width: 80, height: 80, alignment: .center)
+                //                        Spacer()
+                //                    }
+                //                }
+                
+                HStack{
+                    VStack(alignment: .leading) {
                         Text(playlist.title)
                             .font(.title2)
                         Text(String(playlist.year))
+                            .foregroundColor(.secondary)
                     }
                     
                     Spacer()
@@ -50,7 +56,7 @@ struct PlaylistView: View {
                         .font(.system(size: 40))
                         .foregroundColor(.yellow)
                 }
-                .padding()
+                .padding(10)
                 
                 List(playlist.songs, id: \.id) { song in
                     HStack {
@@ -58,24 +64,27 @@ struct PlaylistView: View {
                             .resizable()
                             .frame(width: 40, height: 40, alignment: .center)
                         
-                        HStack {
+                        VStack(alignment: .leading) {
                             Text(song.title)
                             Text(song.artist.name)
+                                .font(.system(size: 15))
+                                .foregroundColor(.secondary)
                         }
-
+                        
                         Spacer()
                         
-                        Image(systemName: "ellipsis")
+                        Image(systemName: "minus.circle")
+                            .foregroundColor(.red)
                     }
                 }.listStyle(PlainListStyle())
                 
                 Spacer()
             }
-            .navigationTitle(playlist.title)
             .navigationBarItems(trailing: Button(action: {
                 print()
             }, label: {
                 Text("Modifier")
+                    .foregroundColor(.yellow)
             }))
         }
     }
